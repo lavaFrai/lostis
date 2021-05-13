0ade52f9da2dc9d11523d8821b3597d664b26fa9
lavaFrai <79652972+lavaFrai@users.noreply.github.com>
2021-05-13 18:34:06 +0300

    Add files via upload

---
+++ index/languages/language_pack.cpp
@@ -0,0 +1,15 @@
+#define EN
+
+
+#ifdef EN
+
+#define LOADING_T ("Loading")
+
+#define MAIN_MENU_T ("Main menu")
+
+#define ABOUT_T ("About")
+#define SHUTDOWN_T ("Shutdown")
+#define SETTINGS_T ("Settings")
+#define EXTERNAL_MODULE ("External module")
+
+#endif
