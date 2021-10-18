a50f94d7d7a42e24e5a8b7cc1069a7766d678930
lavaFrai <vladimirkurdiukovv@yandex.ru>
2021-10-18 23:13:23 +0300

    0.2.18

--- index/index.ino
+++ index/index.ino
@@ -4,7 +4,8 @@
 #include "inc/button.h"
 #include "languages/language_pack.cpp"
 
-namespace defines{
+
+namespace defines {
   #define forever while(1)
   #define DEBUG 1
   #define ERROR_WAIT_TIME 5000
@@ -28,19 +29,72 @@ namespace defines{
   
   #define FOR_i(from, to) for(int i = (from); i < (to); i++)
   #ifdef DEBUG
-    #define log(x) Serial.println(x)
+  #define log(x) Serial.println(x)
   #else
-    #define log(x)
+  #define log(x)
   #endif
 } using namespace defines;
 
-namespace global{
+namespace global {
   U8G2_SSD1306_128X64_NONAME_1_HW_I2C display(U8G2_R0);
 } using namespace global;
 
-namespace kernel{
-  button down(DOWN_BUTTON_PIN), up(UP_BUTTON_PIN), left(LEFT_BUTTON_PIN), right(RIGHT_BUTTON_PIN);
-  uint8_t menu_shift = 0, menu_select = 0, menu_len = 6, menu_selected_pos = 0;;
+namespace apps {
+  byte test()
+  {
+    return 1;
+  }
+
+  byte sisinfo()
+  {
+
+    return 0;
+  }
+}
+
+namespace kernel {
+  button down(DOWN_BUTTON_PIN), up(UP_BUTTON_PIN), left(LEFT_BUTTON_PIN), right(RIGHT_BUTTON_PIN), ok(OK_BUTTON_PIN);
+  uint8_t menu_shift = 0, menu_select = 0, menu_len = 0, menu_selected_pos = 0;
+
+  void error_handler(int code)
+  {
+    if (code)
+    {
+      display.firstPage();
+      do {
+        display.drawFrame(16,16,96,32);
+        display.setFont(main_font);
+        display.setCursor(0, 10);
+        display.print(F("Kernel:"));
+        display.setCursor(18, 26);
+        display.print(F("App finished"));
+        display.setCursor(18, 36);
+        display.print(F("with error code"));
+        display.setCursor(18, 46);
+        display.print(String(code, HEX).length()?F("0"):F(""));
+        display.print(String(code, HEX));
+        display.print('h');
+        display.setCursor(0, 63);
+        display.print(F("reboot device (rst)"));
+      } while (display.nextPage());
+      while(1);
+    }
+  }
+
+  struct menu_item 
+  {
+    char* name;
+    byte (*function)();
+  };
+
+  menu_item menu[] = 
+  {
+    {"sisinfo", apps::sisinfo},
+    {"awknfd", apps::test},
+    {"qwerty", apps::test},
+    {"zxcvbn", apps::test},
+    {"ABOBA", apps::test}
+  };
   
   void boot()
   {
@@ -50,9 +104,9 @@ namespace kernel{
       display.setCursor(0, 8);
       display.print(F("Booting lostis kernel"));
     } while (display.nextPage());
-
+  
     /* Подкачиваем вссе необходимые данные, вобщем грузимся */
-
+  
     display.firstPage();
     do {
       display.setFont(main_font);
@@ -60,56 +114,50 @@ namespace kernel{
       display.print(F("Lostis kernel started"));
     } while (display.nextPage());
     return;
-  }
+}
 
   void run()
   {
-    while (1)
-    {
-      if (down.click()) menu_select++;
-      if (up.click()) menu_select--;
-      if (menu_select == menu_len) menu_select = 0;
-      if (menu_select == 255) menu_select = menu_len - 1;
-      if (menu_select - menu_shift == 4) menu_shift++;
-      if (menu_shift - menu_select == 1) menu_shift--;
-      if (!menu_shift && (menu_select == menu_len - 1)) menu_shift = menu_len - 4;
-      if (menu_shift == menu_len - 4 && menu_select == 0) menu_shift = 0;
-      Serial.print(menu_shift);
-      Serial.print(' ');
-      Serial.print(menu_select);
-      Serial.print(' ');
-      Serial.print(menu_selected_pos);
-      Serial.print('\n');
-      display.firstPage();
-      do {
-        display.setFont(main_font);
-        display.setCursor(37, 7);
-        display.print(F("Main menu"));
-        display.drawHLine(0, 8, 128);
-  
-        FOR_i(0, 4)
+  menu_len = sizeof(menu) / sizeof(menu_item);
+  while (1)
+  {
+    if (down.click()) menu_select++;
+    if (up.click()) menu_select--;
+    if (ok.click()) error_handler(menu[menu_select].function()) ;
+    if (menu_select == menu_len) menu_select = 0;
+    if (menu_select == 255) menu_select = menu_len - 1;
+    if (menu_select - menu_shift == 4) menu_shift++;
+    if (menu_shift - menu_select == 1) menu_shift--;
+    if (!menu_shift && (menu_select == menu_len - 1)) menu_shift = menu_len - 4;
+    if (menu_shift == menu_len - 4 && menu_select == 0) menu_shift = 0;
+    display.firstPage();
+    do {
+      display.setFont(main_font);
+      display.setCursor(37, 7);
+      display.print(F("Main menu"));
+      display.drawHLine(0, 8, 128);
+
+      FOR_i(0, 4)
+      {
+        display.setCursor(0, 21 + 12 * i);
+        if (i + menu_shift == menu_select)
         {
-          display.setCursor(0, 21+12*i);
-          if (i + menu_shift == menu_select)
-          {
-            display.print(F(" > "));
-            display.print("menu item");
-            display.print(i + menu_shift);
-            display.setCursor(109, 21+12*i);
-            display.print(F(" < "));
-          }
-          else
-          {
-            display.print(F("   "));
-            display.print("menu item");
-            display.print(i + menu_shift);
-            display.setCursor(109, 21+12*i);
-            display.print(F("   "));
-          }
+          display.print(F(" > "));
+          display.print(menu[menu_shift + i].name);
+          display.setCursor(109, 21 + 12 * i);
+          display.print(F(" < "));
         }
-      } while (display.nextPage());
-    }
-    return;
+        else
+        {
+          display.print(F("   "));
+          display.print(menu[menu_shift + i].name);
+          display.setCursor(109, 21 + 12 * i);
+          display.print(F("   "));
+        }
+      }
+    } while (display.nextPage());
+  }
+  return;
   }
 } using namespace kernel;
 
